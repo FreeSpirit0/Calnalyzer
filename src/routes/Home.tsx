804be4bc67880616d8b221e62f37ff31306f2041
Poomtum Rattanarat <poomtumratanarat@gmail.com>
2022-11-28 15:17:35 +0700

    Add few method for transforming data.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,8 +3,50 @@ import "../App.css";
 import Content from "../components/Content";
 import Sidebar from "../components/Sidebar";
 import Chart from "../components/Chart";
+import { getForm } from "../services/spoonacular";
+
+type CaloriesFoodRequest = {
+  foodName: string
+}
+
+type FormData = {
+  Age: string,
+  Breakfast: string,
+  Calculate: string,
+  Dinner: string,
+  Exercise: string,
+  FastFood: string,
+  Favourite: string,
+  Gender: string,
+  Height: string,
+  Lunch: string,
+  Weight: string
+}
 
 const Home = () => {
+  const prepareDataForSpooncular = (formData: FormData[]): CaloriesFoodRequest[] => {
+    return formData.map(
+      data => ({
+        foodName: data.Favourite
+      })
+    )
+  }
+
+  const prepareDataForChart = (formData: FormData[]) => {
+    
+  }
+
+  useEffect(() => {
+    getForm().then(data => {
+      console.log(data)
+      console.log(prepareDataForSpooncular(data))
+    })
+
+    return () => {
+      
+    }
+  }, [])
+  
   return (
     <div className="flex bg-gray-50">
       <Sidebar />
