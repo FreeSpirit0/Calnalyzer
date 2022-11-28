370ebe49c3caf2071625efb12f6d7b0601c730d2
Sirawich Tumtamai <tumtamai_w@hotmail.com>
2022-11-28 13:40:56 +0700

    get google form api

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,19 +1,62 @@
-import { useState, useEffect, JSXElementConstructor, ReactElement, ReactFragment, ReactPortal } from 'react'
-import { getRandomFood } from "../services/spoonacular.js";
+import {
+  useState,
+  useEffect,
+  JSXElementConstructor,
+  ReactElement,
+  ReactFragment,
+  ReactPortal,
+} from "react";
+import {
+  getRandomFood,
+  getMealPlan,
+  getForm,
+} from "../services/spoonacular.js";
+
 const Content = () => {
-  const [randomFood, setRandomFood] = useState<any[]>([])
+  const [randomFood, setRandomFood] = useState<any[]>([]);
+  const [mealPlan, setMealPlan] = useState<any>({});
+  const [formData, setFormData] = useState<any[]>([]);
 
   useEffect(() => {
-    getRandomFood().then((data) => {
-      setRandomFood(data.recipes)
-    })
-  }, [])
-  return (
-    <div>Content
-      {randomFood.map((food) => (
-        food.extendedIngredients.map((extended: any) => <p>{extended.name}</p>)
-      ))}</div>
-  )
-}
+    getForm().then((allData) => {
+      allData.map((data: any) => {
+        formData.push(data);
+        console.log("pushed");
+      });
+      console.log(formData);
+      // console.log(formData[2]);
+    });
+  }, []);
 
-export default Content
+  // useEffect(() => {
+  //   getRandomFood().then((data) => {
+  //     setRandomFood(data.recipes);
+  //   });
+  // }, []);
+
+  // useEffect(() => {
+  //   getMealPlan().then((data) => {
+  //     setMealPlan(data);
+  //     console.log(mealPlan);
+  //   });
+  // }, []);
+  return (
+    <div>
+      <div>
+        Content
+        {/* {randomFood.map((food) =>
+            food.extendedIngredients.map((extended: any) => (
+              <p>{extended.name}</p>
+            ))
+          )} */}
+      </div>
+
+      {/* <div>
+        Content
+        {mealPlan.week.monday.meals.map((meal: any) => meal.title)}
+      </div> */}
+    </div>
+  );
+};
+
+export default Content;
--- src/services/spoonacular.js
+++ src/services/spoonacular.js
@@ -2,7 +2,20 @@ import axios from "axios";
 
 export async function getRandomFood() {
   const res = await axios.get(
-    "https://api.spoonacular.com/recipes/random?apiKey=915e7e7a090e42399090d19cb2358e42"
+    "https://api.spoonacular.com/recipes/random?apiKey=915e7e7a090e42399090d19cb2358e42",
+    { params: { number: 30 } }
   );
+  return res.data;
+}
+
+export async function getMealPlan() {
+  const res = await axios.get(
+    "https://api.spoonacular.com/mealplanner/generate?apiKey=915e7e7a090e42399090d19cb2358e42"
+  );
+  return res.data;
+}
+
+export async function getForm() {
+  const res = await axios.get("https://sheetdb.io/api/v1/cmqnl8n0lcadt");
   return res.data;
 }
